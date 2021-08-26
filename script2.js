034b04b87a13d8244aef5d22f911d2d2008bba87
Alexan345 <87387229+Alexan345@users.noreply.github.com>
2021-08-26 11:40:41 +0300

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,288 @@
+/*let a = 10;
+
+if(a == 10){
+  console.log('True');
+}else {
+  console.log('False');
+}*/
+
+
+/*let a = -3;
+
+if(a == 0){
+  console.log('True');
+}else{
+  console.log('False');
+}*/
+
+/*let a = 3;
+
+if(a > 0 ){
+  console.log('True');
+}else{
+  console.log('False');
+}*/
+
+
+/*let a = -3;
+
+if(a < 0){
+  console.log('True');
+}else {
+  console.log('False');
+}*/
+
+/*let a = 2;
+
+if(a >= 0){
+  console.log('True');
+}else {
+  console.log('False');
+}*/
+
+/*let a = -3;
+
+if(a <= 0){
+  console.log('True');
+}else{
+  console.log('False');
+}*/
+
+/*let a = -3;
+
+if(a != 0){
+  console.log('True');
+}else{
+  console.log('False');
+}*/
+
+/*let a = 'TEST';
+
+if(a == 'test'){
+  console.log('True');
+}else{
+  console.log('False');
+}*/
+
+
+/*let a = 3;
+
+if(a == 1 && a == '1'){
+  console.log('True');
+}else{
+  console.log('False');
+}*/
+
+/*let test = 'false';
+
+if(test == 'true'){
+  console.log('True');
+}else{
+  console.log('False');    aici mai completez 2 varianta
+}*/
+
+/*let test = 'ansbdd';
+let exam = (test == 'true') ? console.log('True') : console.log('False'); ----operator ternat */ 
+ 
+
+
+
+/* && || */
+
+/*let a = 6;
+
+if(a > 0 && a < 5 ){
+  console.log('True');
+}else {
+  console.log('False');
+}*/
+
+
+
+/*let a = 2;
+
+if(a == 0 || a == 2){
+  console.log(a + 7);
+}else {
+  console.log(a / 10);
+}*/
+
+/*let a = 3;
+let b = 5;
+
+if(a == 1 || a < 1, b > 3 || b == 3 ){
+  console.log(a + b);          
+}else{
+  console.log(b - a);
+}*/
+
+/*let a = '1';
+let b = '5';
+
+if( (a > 2 && a < 11) || ( b >= 6 && b < 14)){
+  console.log('True');
+}else{
+  console.log('False');
+}*/
+
+
+
+
+/*switch-case*/
+
+
+
+/*let num = 4;
+
+switch(num){
+  case 1:
+    console.log('winter');
+  break;
+  case 2:
+    console.log('Spring');
+  break;
+  case 3:
+    console.log('Summer');
+  break;
+  case 4: 
+    console.log('Autumn');
+  break;
+  
+}*/
+
+
+/*let day = 23;
+
+if(day >= 1 && day <= 10){
+  console.log('decada 1');
+}else if(day <= 20){
+  console.log('decada 2');
+}else if(day <= 31){
+  console.log('decada 3');
+}*/
+
+
+/*let month = 11;
+
+
+if (month >= 1 && month <=2 || month == 12){
+  console.log('winter');
+}else if(month > 2 && month <= 5){
+  console.log('spring');
+}else if(month > 6 && month <= 8){
+  console.log('summer');
+}else if(month > 8 && month <= 11){
+  console.log('autumn');
+} */
+
+
+/*let str = 'bcdea';
+
+if(str[0] == 'a'){
+  console.log('yes');
+}else{
+  console.log('no');
+}*/
+
+
+
+
+/*let num = '12345';
+
+if(num[0] =='1' || num[0] =='2' || num[0] =='3'){   mai intreb aici de ce fara'' nu se primeste
+  console.log('yes');           
+}else{
+  console.log('no');
+}*/
+
+
+
+  /*let str = '453';
+  console.log(+str[0] + +str[1] + +str[2]);*/     
+
+
+
+  /*let num = '153153';
+
+  if((+num[0] + +num[1] + +num[2]) == (+num[3] + +num[4] + +num[5])){       am intrebari, cum se poate fara String ?
+    console.log('yes');
+  }else{
+    console.log('no');
+  }*/                   
+
+
+
+
+
+
+
+  /*while & for */
+
+
+  /*let num = 1;
+  while(num <= 100){
+    console.log(num);
+    num++;
+  }*/
+
+  /*for( let num = 1; num <= 100; num++ ){
+    console.log(num);
+  }*/
+
+
+
+  /*let num = 11;
+  
+  while(num <=33){
+    console.log(num);
+    num++;
+  }*/
+
+  /*for(let num = 11; num <= 33; num++){
+    console.log(num);*/
+
+    
+    
+    /*let num = [ 1, 2, 3, 4, 5];
+     
+    for(n = 0; n < num.length; n++){   
+      console.log(num[n]);
+    }*/
+
+    /*for(let num = 0; num <= 100; num++){
+     if( num % 2 == 0){
+        console.log(num);
+      }
+    }*/
+
+    /*let arr = [1,2,5,9,4,13,4,10];
+    console.log(arr.length);
+    
+
+    for(let num = arr[0]; num <= arr.length; num++ ){
+      if(arr[num] == 4){
+        console.log('Nope!');
+      continue;
+      }else{
+        console.log('Yeap!')
+      }break;
+    }*/
+    
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
